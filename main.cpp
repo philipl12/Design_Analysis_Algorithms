9553c0e53dba6000ad1418e75b4c14004b7a155e
philipl12 <philiplin512@gmail.com>
2018-11-05 22:35:21 -0500

    Need to finish up a few more methods.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <bits/stdc++.h>
+#include <cmath>
 using namespace std;
 
 class xyCoord;
@@ -26,6 +27,7 @@ public:
     KMean(int nRows, int nCols, int minV, int maxV, int K) {
         imgAry = new int*[nRows];
         for (int i = 0; i < nRows; i++) imgAry[i] = new int[nCols];
+
         KCentroids = new xyCoord[K + 1];
         numRows = nRows;
         numCols = nCols;
@@ -65,6 +67,84 @@ public:
             if (KCount > K) KCount = 1;
         }
     }
+
+    void point2Image() {
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                imgAry[i][j] = 0;
+            }
+        }
+        for (int i = 0; i < numPts; i++) {
+            int j = pointSet[i].xCoord;
+            int k = pointSet[i].yCoord;
+            imgAry[j][k] = pointSet[i].label;
+        }
+    }
+
+    void printImage(ofstream &outFile) {
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (imgAry[i][j] > 0) outFile << imgAry[i][j];
+                else { outFile << ' '; }
+            }
+            outFile << endl;
+        }
+    }
+
+    void computeCentroids() {
+        int *sumX = new int[K];
+        int *sumY = new int[K];
+        int *totalPt = new int[K];
+        int index = 0, label;
+
+        for (int i = 0; i < K; i++) {
+            sumX[i] = 0;
+            sumY[i] = 0;
+            totalPt[i] = 0;
+        }
+
+        while (index < numPts) {
+            label = pointSet[index].label;
+            sumX[label] += pointSet[index].xCoord;
+            sumY[label] += pointSet[index].yCoord;
+            totalPt[label]++;
+            index++;
+        }
+        /*
+        for (int index = 0; index < numPts; index++) {
+            label = pointSet[index].label;
+            sumX[label] += pointSet[index].xCoord;
+            sumY[label] += pointSet[index].yCoord;
+            totalPt[label]++;
+        }
+        */
+        label = 1;
+        while (label <= K) {
+            KCentroids[label].xCoord = (sumX[label]/ totalPt[label]);
+            KCentroids[label].yCoord = (sumX[label]/ totalPt[label]);
+            label++;
+        }
+        /*
+        for (label = 1; label <= K; label++) {
+            KCentroids[label].xCoord = (sumX[label]/ totalPt[label]);
+            KCentroids[label].yCoord = (sumX[label]/ totalPt[label]);
+        }
+        */
+    }
+
+    double computeDist(Point p1, Point p2) {
+        int xSquared = (p2.xCoord - p1.xCoord) * (p2.xCoord - p1.xCoord);
+        int ySquared = (p2.yCoord - p1.yCoord) * (p2.yCoord - p1.yCoord);
+        double distance = sqrt(xSquared + ySquared);
+        return distance;
+    }
+
+    void kMeansClustering() {
+        int iteration = 0;
+        assignLabel();
+
+    }
 };
 
 int main(int argc, char** argv) {
@@ -96,7 +176,7 @@ int main(int argc, char** argv) {
     ifstream outFile_1;
     outFile_1.open(argv[2]);
     test.loadPointSet(outFile_1);
-    test.assignLabel();
+    //test.kMeansClustering();
 
     for (int i = 0; i < test.numRows; i++) delete[] test.imgAry[i];
     delete[] test.imgAry;
