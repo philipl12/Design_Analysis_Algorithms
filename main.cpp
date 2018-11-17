19ba901022e2efb56a0cbd965275cf4a5021c691
philipl12 <philiplin512@gmail.com>
2018-11-17 16:34:56 -0500

    Need to fix checkCycle method for logical flaws

--- main.cpp
+++ main.cpp
@@ -25,9 +25,9 @@ public:
         open = new Node();
     }
 
-    bool checkJobDone() {
+    bool checkJobDone(int someArray[]) {
         for (int i = 1; i < numNodes + 1; i++) {
-            if (jobDone[i] == 0) return true;
+            if (someArray[i] == 0) return true;
         }
         return false;
     }
@@ -103,6 +103,13 @@ public:
         return spot;
     }
 
+    Node* removeNode(Node *head) {
+        Node *removedNode = head->next;
+        head->next = head->next->next;
+        removedNode->next = NULL;
+        return removedNode;
+    }
+
     void printList() {
         Node *spot = open;
         do {
@@ -120,17 +127,43 @@ public:
         }
         return -1;
     }
+
+    void updateTable(int availProc, int currentTime, Node *newJob) {
+        //int expectedTime = currentTime + newJob->jobTime;
+        for (int i = currentTime; i <= (currentTime + newJob->jobTime); i++) {
+            scheduleTable[availProc][i] = newJob->jobTime;
+        }
+    }
+
+    int checkCycle() {
+        if (open->next == NULL) {
+            if (checkJobDone(jobDone)) {
+                if (checkJobDone(processTime)) return 1;
+            }
+        }
+        return 0;
+    }
+
+    void printTable(ofstream &outFile, int procGiven) {
+        for (int i = 0; i <= procGiven; i++) {
+            if (i > 0) cout << "P(" << i << ")";
+            for (int j = 0; j <= totalJobTimes; j++) {
+
+            }
+        }
+    }
 };
 
 int main(int argc, char const *argv[]) {
     ifstream inFile1, inFile2;
-    ofstream outFile;
+    ofstream outFile1, outFile2;
     int numNodes, availProc, procGiven, procUsed = 0, currentTime = 0, orphanNode = 0;
     Scheduling test = Scheduling();
 
     inFile1.open(argv[1]);
     inFile2.open(argv[2]);
-    outFile.open(argv[3]);
+    outFile1.open(argv[3]);
+    outFile2.open(argv[4]);
 
     inFile1 >> numNodes;
     test.adjacencyMatrix = new int*[numNodes + 1];
@@ -158,7 +191,7 @@ int main(int argc, char const *argv[]) {
 
     test.initializeArrays();
 
-    while (test.checkJobDone()) {
+    while (test.checkJobDone(test.jobDone)) {
         while (orphanNode != -1 ) {
             orphanNode = test.getUnmarkedOrphan();
             if (orphanNode == -1) break;
@@ -169,18 +202,30 @@ int main(int argc, char const *argv[]) {
             //cout << newNode->jobID << newNode->jobTime << endl;
             test.insert2Open(test.open, newNode);
         }
-        //test.printList();
-        availProc = test.findProcessor(procGiven);
-        //cout << availProc << endl;
-        if (availProc > 0) {
-            procUsed++;
-            //Node *newJob =
+        test.printList();
+        while (test.open->next != NULL && procUsed < procGiven) {
+            availProc = test.findProcessor(procGiven);
+            if (availProc > 0) {
+                procUsed++;
+                Scheduling::Node *newJob = test.removeNode(test.open);
+                test.processJob[availProc] = newJob->jobID;
+                test.processTime[availProc] = newJob->jobTime;
+                test.updateTable(availProc, currentTime, newJob);
+            }
         }
+/*
+        if (test.checkCycle() >= 1) {
+            cout << "Cycle in the graph\n";
+            exit(1);
+        }
+*/
+        test.printTable(outFile1, procGiven);
     }
 
 
     inFile1.close();
     inFile2.close();
-    outFile.close();
+    outFile1.close();
+    outFile2.close();
     return 0;
 }
