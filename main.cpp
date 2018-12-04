a103f72d0e16601aca03e49dd2db3d930518943c
philipl12 <philiplin512@gmail.com>
2018-12-03 21:17:00 -0500

    Small class changes

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 #include <fstream>
 using namespace std;
 
+class AStarNode {
+public:
+    int ID, numEdgeFromStart = 0, gStar, hStar, fStar;
+    AStarNode *next = NULL, *parent = NULL;
+
+    AStarNode(int ID, int gStar, int hStar) {
+        this->ID = ID;
+        this->gStar = gStar;
+        this->hStar = hStar;
+        fStar = gStar + hStar;
+    }
+};
+
+class AStarSearch {
+public:
+    int numNodes, start, whichHFunction;
+    int costMatrix;
+};
+
 int main(int argc, char const *argv[]) {
     ifstream input1;
     ofstream outFile1, outFile2;
@@ -10,6 +29,7 @@ int main(int argc, char const *argv[]) {
     input1.open(argv[1]);
     outFile1.open(argv[2]);
     outFile2.open(argv[3]);
+
 
 
     input1.close();
