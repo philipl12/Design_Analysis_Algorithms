cf68a1ad20d0c65fd3b4ba1996e7164099c14d48
philipl12 <philiplin512@gmail.com>
2019-07-16 21:56:07 -0400

    insertion sort

--- Java/Sorting/src/Sorts.java
+++ Java/Sorting/src/Sorts.java
@@ -1,24 +1,41 @@
 
 public class Sorts {
-	public static void bubbleSort(int[] a) {
-		int max = a[0], n = a.length;
-		
-		for (int i = 0; i < n - 1; i++) {
-			for (int j = i + 1; j < n - i - 1; i++) {
-				if (a[max] > a[j]) {
-					int temp = a[max];
-					a[max] = a[j];
-					a[j] = temp;
-				}
-			}
-		}
-		
-		//printArray(a);
-	}
 	
 	private static void printArray(int[] a) {
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
+		}
+	}
+	
+	public static void bubbleSort(int[] a) {
+		int n = a.length;
+		System.out.println("Bubble Sort");
+		
+		for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (a[j] > a[j+1]) { 
+                    int temp = a[j]; 
+                    a[j] = a[j+1]; 
+                    a[j+1] = temp; 
+                }
+			}
+		}
+		printArray(a);
+	}
+	
+	public static void insertionSort(int[] a) {
+		int n = a .length;
+		System.out.println("Insertion sort");
+		
+		for (int i = 1; i < n - 1; i++) {
+			int value = a[i], j = i - 1;
+			
+			while (j >= 0 && a[j] > value) {
+				a[j] = a[j + 1];
+				j--;
+			}
+			
+			a[j + 1] = value;
 		}
 	}
 }
--- Java/Sorting/src/Test.java
+++ Java/Sorting/src/Test.java
@@ -4,15 +4,60 @@ import java.io.PrintStream;
 import java.util.Random;
 
 public class Test {
-
+	
+	private static void printArray(int[] a) {
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+		
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void bubbleSort(int[] a) {
+		int n = a.length;
+		System.out.println("Bubble Sort");
+		
+		for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (a[j] > a[j+1]) 
+                { 
+                    int temp = a[j]; 
+                    a[j] = a[j+1]; 
+                    a[j+1] = temp; 
+                } 
+		
+		printArray(a);
+	}
+	
+	public static void insertionSort(int[] a) {
+		int n = a .length;
+		System.out.println("Insertion sort");
+		
+		for (int i = 1; i < n - 1; i++) {
+			int value = a[i], j = i - 1;
+			
+			while (j >= 0 && a[j] > value) {
+				a[j] = a[j + 1];
+				j--;
+			}
+			
+			a[j + 1] = value;
+		}
+		
+		printArray(a);
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		Random rand = new Random();
 		PrintStream out = new PrintStream(new File("output.txt"));
-		PrintStream console = System.out;
+		//PrintStream console = System.out;
 		
 		System.setOut(out);
 		
-		int[] numTestCases = {10, 100};
+		int[] numTestCases = {10, 100, 1000, 10000};
 		
 		
 		try {
@@ -22,16 +67,21 @@ public class Test {
 				for (int j = 0; j < testArray.length; j++) {
 					
 					testArray[j] = rand.nextInt(numTestCases[i]);
-					System.out.print(testArray[j] + " ");
+					//System.out.print(testArray[j] + " ");
 					
 				}
-				System.out.println();
-				Sorts.bubbleSort(testArray);
-				
+				/*
 				for (int h = 0; h < testArray.length; h++) {
 					System.out.print(testArray[h] + " ");
 				}
 				System.out.println();
+				*/
+				Test.bubbleSort(testArray);
+				
+				//Test.insertionSort(testArray);
+				
+
+				
 			}
 			
 			out.close();
